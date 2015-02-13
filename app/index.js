56b88778d965a9bc2a4cbea53cf72c4191f1339e
Phil Moody <Phil@Server.local>
2015-02-13 14:39:36 -0500

    fixes .touch

--- app/index.js
+++ app/index.js
@@ -139,8 +139,8 @@ module.exports = yeoman.generators.Base.extend({
           this.destinationPath('assets/css/style.css')
         );
 
-        this.touch('assets/css/global/_variable.scss');
-        this.touch('assets/css/global/_fonts.scss');
+        this.write('assets/css/global/_variable.scss');
+        this.write('assets/css/global/_fonts.scss');
 
       };
 
