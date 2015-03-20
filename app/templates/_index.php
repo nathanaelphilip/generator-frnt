0891813fbfc02bf83a816f57294953cbe643c162
Nathanael Moody <dev@stitchdesignco.com>
2015-03-20 16:57:50 -0400

    adds initial-scale=1 to viewport

--- app/templates/_index.php
+++ app/templates/_index.php
@@ -4,7 +4,7 @@
     <title><%= title %></title>
     <!-- meta -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
     <!-- css -->
     <link rel="stylesheet" href="assets/css/helpers.css">
     <link rel="stylesheet" href="assets/css/style.css">
