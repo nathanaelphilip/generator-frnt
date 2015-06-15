3774879b177e02e71dad28cb33d5bda9be658651
Nathanael Moody <dev@stitchdesignco.com>
2015-06-15 13:03:43 -0400

    minor fixes

--- app/templates/_gulp.js
+++ app/templates/_gulp.js
@@ -44,7 +44,7 @@ gulp.task('js', function() {
 gulp.task('img', function() {
 
   return gulp.src(paths.img.src)
-    .pipe(imagemin({
+    .pipe($.imagemin({
       progressive: true
     }))
     .pipe(gulp.dest(paths.img.dest));
