afb425217a88c723c175151d6a871fc18d913772
Nathanael Moody <hello@nathanaelphilip.com>
2016-04-05 12:15:33 -0400

    uses imgagemin; fix paths issues

--- app/templates/_gulp.js
+++ app/templates/_gulp.js
@@ -1,7 +1,10 @@
 var gulp = require("gulp");
 var elixir = require("laravel-elixir");
+var imagemin = require("laravel-elixir-imagemin");
 
 elixir(function(mix){
-    mix.sass("app.scss");
-    mix.scripts(['app.js']);
+    mix.sass("app.scss")
+       .scripts(['app.js'])
+       .imagemin()
+       .browserSync({proxy: 'http://'});
 });
--- app/templates/_index.php
+++ app/templates/_index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="icon" type="image/png" href="assets/images/favicon.png">
+    <link rel="icon" type="image/png" href="public/images/favicon.png">
     <!-- Disable tap highlight on IE -->
     <meta name="msapplication-tap-highlight" content="no">
     <!-- Web Application Manifest -->
@@ -12,18 +12,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <!-- css -->
-    <link rel="stylesheet" href="assets/css/helpers.css">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="public/css/app.css">
     <!-- fonts -->
     <!-- scripts -->
-    <!--[if IE]><script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script><![endif]-->
     <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
 </head>
 <body>
     <main>
-        
+
     </main>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.1/jquery.min.js"></script>
-	<script src="assets/js/app.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+	<script src="public/js/all.js"></script>
 </body>
 </html>
